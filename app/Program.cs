bda918d0a6f01af3a864cd4f2370ebef9d0a5860
FelMulta <multarifilippo@gmail.com>
2023-04-04 18:38:41 +0200

    punto 1 semaforo verde

--- app/Program.cs
+++ app/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EdCivicaMultariPozzato
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inserisci la stringa da cifrare: ");
+            string s = Console.ReadLine();
+            Console.WriteLine("Inserisci il numero di posizioni da spostare: ");
+            int n = int.Parse(Console.ReadLine());
+
+            string risultato = Cifratura(s, n);
+            Console.WriteLine(risultato);
+        }
+
+        public static string Cifratura(string s, int n)
+        {
+            string alfabeto = "abcdefghijklmnopqrstuvwxyz";
+            string risultato = "";
+
+            foreach (char c in s)
+            {
+                int posizione = alfabeto.IndexOf(c);
+                if (posizione >= 0)
+                {
+                    posizione = (posizione + n) % alfabeto.Length;
+                    risultato += alfabeto[posizione];
+                }
+                else
+                {
+                    risultato += c;
+                }
+            }
+            return risultato;
+        }  
+    }
+}
