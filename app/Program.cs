8a4de8c24d8bda7addc89528278ce20cb1f2696a
FelMulta <multarifilippo@gmail.com>
2023-04-05 18:22:55 +0200

    Punto 6 semaforo rosso

--- app/Program.cs
+++ app/Program.cs
@@ -10,149 +10,148 @@ namespace EdCivicaMultariPozzato
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Funzione da eseguire: ");
-            int smistamento = int.Parse(Console.ReadLine());
             string s, risultato;
             int n;
             int sum  = 0;
             double product = 1.0; 
-            switch (smistamento){
-                case 1: 
-                    Console.WriteLine("Inserisci la stringa da cifrare: ");
-                    s = Console.ReadLine();
-                    Console.WriteLine("Inserisci il numero di posizioni da spostare: ");
-                    n = int.Parse(Console.ReadLine());
-
-                    risultato = Cifratura(s, n);
-                    Console.WriteLine(risultato);
-                    break;
-                case 2:
-                    Console.WriteLine("Inserisci la stringa da decifrare: ");
-                    s = Console.ReadLine();
-                    Console.WriteLine("Inserisci il numero di posizioni da spostare: ");
-                    n = int.Parse(Console.ReadLine());
-
-                    risultato = Decifratura(s, n);
-                    Console.WriteLine(risultato);
-                    break;
-                case 3:
-                    Console.WriteLine("Inserisci la stringa da decifrare: ");
-                    s = Console.ReadLine();
-                    Console.WriteLine("Inserisci il numero di posizioni da spostare: ");
-                    n = int.Parse(Console.ReadLine());
-
-                    risultato = SostituzioneLettereValori(s, n, sum);
-                    Console.WriteLine(risultato);
-                    break;
-                case 4:
-                    Console.WriteLine("Inserisci la stringa da decifrare: ");
-                    s = Console.ReadLine();
-                    Console.WriteLine("Inserisci il numero di posizioni da spostare: ");
-                    n = int.Parse(Console.ReadLine());
-
-                    risultato = SostituzioneLettereValoriDivisione(s, n, product);
-                    Console.WriteLine(risultato);
-                    break;
-                case 5:
-                    Console.WriteLine("Inserisci la stringa da decifrare: ");
-                    s = Console.ReadLine();
-                    Console.WriteLine("Inserisci il numero di posizioni da spostare: ");
-                    n = int.Parse(Console.ReadLine());
-
-                    risultato = Punto5(s, n, sum, product);
-                    Console.WriteLine(risultato);
-                    break;
-            }
-        }
-
-        public static string Cifratura(string s, int n)
-        {
-            string alfabeto = "abcdefghijklmnopqrstuvwxyz";
-            string risultato = "";
-
-            foreach (char c in s)
+            int contatore = 0;
+            bool chiuso = false;
+            do
             {
-                int posizione = alfabeto.IndexOf(c);
-                if (posizione >= 0)
-                {
-                    posizione = (posizione + n) % alfabeto.Length;
-                    risultato += alfabeto[posizione];
-                }
-                else
-                {
-                    risultato += c;
-                }
-            }
-            return risultato;
-        }  
+                Console.WriteLine("Funzione da eseguire: ");
+                int smistamento = int.Parse(Console.ReadLine());
+                switch (smistamento){
+                    case 1: 
+                        Console.WriteLine("Inserisci la stringa da cifrare: ");
+                        s = Console.ReadLine();
+                        Console.WriteLine("Inserisci il numero di posizioni da spostare: ");
+                        n = int.Parse(Console.ReadLine());
 
-        public static string Decifratura(string s, int n)
-        {
-            string alfabeto = "abcdefghijklmnopqrstuvwxyz";
-            string risultato = "";
+                        risultato = Cifratura(s, n);
+                        Console.WriteLine(risultato);
+                        break;
+                    case 2:
+                        Console.WriteLine("Inserisci la stringa da decifrare: ");
+                        s = Console.ReadLine();
+                        Console.WriteLine("Inserisci il numero di posizioni da spostare: ");
+                        n = int.Parse(Console.ReadLine());
 
-            foreach (char c in s)
-            {
-                int posizione = alfabeto.IndexOf(c);
-                if (posizione >= 0)
-                {
-                    posizione = (posizione - n + alfabeto.Length) % alfabeto.Length;
-                    risultato += alfabeto[posizione];
-                }
-                else
-                {
-                    risultato += c;
-                }
-            }
-            return risultato;
-        }
-        public static string SostituzioneLettereValori(string s, int n, int sum) 
-        {
-            foreach (char c in s) 
-            {
-                int value = (int) char.ToUpper(c) - 64;
-                if (value < 1 || value > 26) 
-                {
-                    Console.WriteLine("La stringa deve contenere solo lettere dell'alfabeto.");
-                }
-                sum += value;
-            }
-            int result = sum * n;
-            Console.WriteLine("Risultato: " + result);
-            string stringResult = result.ToString();
-            return  stringResult;
-        }
+                        risultato = Decifratura(s, n);
+                        Console.WriteLine(risultato);
+                        break;
+                    case 3:
+                        Console.WriteLine("Inserisci la stringa da decifrare: ");
+                        s = Console.ReadLine();
+                        Console.WriteLine("Inserisci il numero di posizioni da spostare: ");
+                        n = int.Parse(Console.ReadLine());
 
-        public static string SostituzioneLettereValoriDivisione(string s, int n, double product)
-        {
-            foreach (char c in s) 
-            {
-                int value = (int) char.ToUpper(c) - 64;
-                if (value < 1 || value > 26) 
-                {
-                    Console.WriteLine("La stringa deve contenere solo lettere dell'alfabeto.");
+                        risultato = SostituzioneLettereValori(s, n, sum);
+                        Console.WriteLine(risultato);
+                        break;
+                    case 4:
+                        Console.WriteLine("Inserisci la stringa da decifrare: ");
+                        s = Console.ReadLine();
+                        Console.WriteLine("Inserisci il numero di posizioni da spostare: ");
+                        n = int.Parse(Console.ReadLine());
+
+                        risultato = SostituzioneLettereValoriDivisione(s, n, product);
+                        Console.WriteLine(risultato);
+                        break;
+                    case 5:
+                        Console.WriteLine("Inserisci la stringa da decifrare: ");
+                        s = Console.ReadLine();
+                        Console.WriteLine("Inserisci il numero di posizioni da spostare: ");
+                        n = int.Parse(Console.ReadLine());
+
+                        risultato = Punto5(s, n, sum, product);
+                        Console.WriteLine(risultato);
+                        break;
+                    case 6:
+                        Console.WriteLine("Hai usato la funzione 5 " + contatore + " volte.");
+                        break;
                 }
-                product *= value;
-            }
-            double result = Math.Ceiling(product / n);
+            
 
-            Console.WriteLine("Risultato: " + result);
-            string stringResult = result.ToString();
-            return  stringResult;
-        }
+                static string Cifratura(string s, int n)
+                {
+                    string alfabeto = "abcdefghijklmnopqrstuvwxyz";
+                    string risultato = "";
 
-        public static string Punto5(string s, int n, int sum, double product)
-        {
-            if(n % 2 == 0)
-            {
-                string risultato = SostituzioneLettereValori(s, n, sum);
-                return risultato;
-            }
-            else
-            {
-                string risultato = SostituzioneLettereValoriDivisione(s, n, product);
-                return risultato;
-            }
+                    foreach (char c in s)
+                    {
+                        int posizione = alfabeto.IndexOf(c);
+                        if (posizione >= 0)
+                        {
+                            posizione = (posizione + n) % alfabeto.Length;
+                            risultato += alfabeto[posizione];
+                        }
+                        else
+                        {
+                            risultato += c;
+                        }
+                    }
+                    return risultato;
+                }  
+
+                static string Decifratura(string s, int n)
+                {
+                    string alfabeto = "abcdefghijklmnopqrstuvwxyz";
+                    string risultato = "";
+
+                    foreach (char c in s)
+                    {
+                        int posizione = alfabeto.IndexOf(c);
+                        if (posizione >= 0)
+                        {
+                            posizione = (posizione - n + alfabeto.Length) % alfabeto.Length;
+                            risultato += alfabeto[posizione];
+                        }
+                        else
+                        {
+                            risultato += c;
+                        }
+                    }
+                    return risultato;
+                }
+                static string SostituzioneLettereValori(string s, int n, int sum) 
+                {
+                    foreach (char c in s) 
+                    {
+                        int value = (int) char.ToUpper(c) - 64;
+                        if (value < 1 || value > 26) 
+                        {
+                            Console.WriteLine("La stringa deve contenere solo lettere dell'alfabeto.");
+                        }
+                        sum += value;
+                    }
+                    int result = sum * n;
+                    Console.WriteLine("Risultato: " + result);
+                    string stringResult = result.ToString();
+                    return  stringResult;
+                }
+
+                static string SostituzioneLettereValoriDivisione(string s, int n, double product)
+                {
+                    foreach (char c in s) 
+                    {
+                        int value = (int) char.ToUpper(c) - 64;
+                        if (value < 1 || value > 26) 
+                        {
+                            Console.WriteLine("La stringa deve contenere solo lettere dell'alfabeto.");
+                        }
+                        product *= value;
+                    }
+                    double result = Math.Ceiling(product / n);
+
+                    Console.WriteLine("Risultato: " + result);
+                    string stringResult = result.ToString();
+                    return  stringResult;
+                }
+
+                string Punto5(string s, int n, int sum, double product)
+                {
+                
+            }while(!chiuso);      
         }
     }
 }
